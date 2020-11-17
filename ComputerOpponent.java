0c3529a2857fc8e1dd61e98c61c47de653410164
Matt Morris <mattmorris120@gmail.com>
2020-11-17 12:24:12 -0500

    Update ComputerOpponent.java
    Finished OPPONENT

--- ComputerOpponent.java
+++ ComputerOpponent.java
@@ -1,24 +1,8 @@
 /**
- * This class gives you the computer's move in Rock, Paper, Scissors.
+ * This class random generates the  computer's move in Rock, Paper, Scissors.
  */
 public class ComputerOpponent {
-
-    private static boolean TESTING_MODE = false;
-    private static int cycler = 0;
-
     public static String getMove() {
-        if (TESTING_MODE) {
-            switch (cycler++ % 3) {
-                case 0:
-                    return "rock";
-                case 1:
-                    return "paper";
-                default: // (case 2)
-                    return "scissors";
-            }
-        }
-
-        else {
             switch ((int) (Math.random() * 3)) {
                 case 0:
                     return "rock";
@@ -27,8 +11,8 @@ public class ComputerOpponent {
                 default: // (case 2)
                     return "scissors";
             }
+        
+    
         }
-    }
-
 
 }
