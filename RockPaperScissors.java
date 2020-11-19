d2492023a8af87dd0a63bddad9dc18e09ab63886
Matt Morris <73907438+MattMorris247@users.noreply.github.com>
2020-11-19 12:22:29 -0500

    Update RockPaperScissors.java

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -3,25 +3,10 @@
  * RockPaperScissors.java
  * Author:  Matt Morris  
  * Submission Date:  17 February 2020.
- *
  * Purpose: This program simulates a game of Rock Paper Scissors. The program asks the user for their choice, (the input) 
  * then matches it against a computer generated move to see who wins. Program uses a loop, that allows the user to the play
  * a set number winning matches to complete the game. 
  *
- * Statement of Academic Honesty:
- *
- * The following code represents my own work. I have neither
- * received nor given inappropriate assistance. I have not copied
- * or modified code from any source other than the course webpage
- * or the course textbook. I recognize that any unauthorized
- * assistance or plagiarism will be handled in accordance with
- * the University of Georgia's Academic Honesty Policy and the
- * policies of this course. I recognize that my work is based
- * on an assignment created by the Department of Computer
- * Science at the University of Georgia. Any publishing 
- * or posting of source code for this assignment is strictly
- * prohibited unless you have written consent from the Department
- * of Computer Science at the University of Georgia.  
  */
 import java.util.Scanner;
 
